93806fc0a4f76769754e25a42c6bd658f2671a14
igordonin <igordonin@gmail.com>
2022-07-12 11:21:45 -0300

    feat: add roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { App } from './app';
+import './index.css';
+import './fonts/Roboto-Regular.ttf';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />} />
+      <Route path="/" element={<App />} />
     </Routes>
   </BrowserRouter>
 );
