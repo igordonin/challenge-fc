8a5a30fa37dc373689185ffce2b7c5c520a79b91
igordonin <igordonin@gmail.com>
2022-07-12 17:46:17 -0300

    refactor: rename the settings interface for better semantics

--- src/store/settings-store.ts
+++ src/store/settings-store.ts
@@ -1,14 +1,14 @@
 import create from 'zustand';
 
-interface Settings {
+interface SettingsStore {
   hour12: boolean;
   toggleHour12: () => void;
 }
 
-export const useSettingsStore = create<Settings>((set) => ({
+export const useSettingsStore = create<SettingsStore>((set) => ({
   hour12: true,
   toggleHour12: () =>
-    set((state: Settings): Settings => {
+    set((state: SettingsStore): SettingsStore => {
       return {
         ...state,
         hour12: !state.hour12,
